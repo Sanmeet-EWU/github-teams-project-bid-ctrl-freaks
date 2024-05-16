dbf9ba5e3aa8c5b90a33c68276da4631219deac3
Brady <bdullanty@ewu.edu>
2024-05-16 10:47:19 -0700

    Initial Vue File Transfer
    Moved files from Erics Github to group Github

--- App-files/README.md
+++ App-files/README.md
@@ -1,1 +1,37 @@
-empty until i have time to add instruction - Eric
+<b> Install instruction </b> <br/>
+-Install node.js <br/>
+-Install Android Studio (optional if you want to run Android emulator)<br/>
+
+<br/>
+<br/>
+
+Go to command prompt and enter: <br/>
+-npm install -g @vue/cli <br/>
+(Installing Vue) <br/>
+-npm install -g @ionic/cli @capacitor/assets <br/>
+(Installing Ionic and capacitor to make emulator) <br/>
+-CD in command prompt to where you want to put the folder <br/>
+-git clone https://github.com/tienv9/weather-freak.git weather-freak <br/>
+(clone my repo) <br/>
+-cd weather-freak && npm install && ionic serve <br/>
+(run this command should open up the app in brower. If not then you're missing vue or ionic) <br/>
+<br/>
+You should now be good to open the <b>entire folder</b> with a code editor like VScode (required Vue, Typescript, and Ionic extension) to edit the code within the src folder <br/>
+
+<br/>
+<br/>
+<br/>
+
+<b>Emulator Instruction</b> <br/>
+In Terminal of the project and run: <br/>
+*Remember to have Android Studio install and open during first run to build  <br/>
+-Ionic cap add android <br/>
+(add capacitor android files) <br/>
+-Ionic cap build android <br/>
+(This should open up Android Studio and build the emulator) <br/>
+-Ionic cap run <br/>
+(Should open up the emulator by itself *may take a while) <br/>
+
+
+
+
---
+++ App-files/src/pages/Setting.vue
@@ -0,0 +1,63 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Settings</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>
+          <ion-label>Dark Mode</ion-label>
+          <ion-toggle slot="end"></ion-toggle>
+        </ion-item>
+        <ion-item>
+          <ion-label>Notifications</ion-label>
+          <ion-toggle slot="end"></ion-toggle>
+        </ion-item>
+        <ion-item>
+          <ion-label>GPS</ion-label>
+          <ion-toggle slot="end"></ion-toggle>
+        </ion-item>
+        <ion-item>
+          <ion-label>Location</ion-label>
+          <ion-select slot="end" interface="action-sheet" placeholder="Spokane, WA">
+            <ion-select-option value="spokane">Spokane, WA</ion-select-option>
+            <ion-select-option value="seattle">Seattle, WA</ion-select-option>
+            <ion-select-option value="portland">Portland, OR</ion-select-option>
+            <ion-select-option value="boise">Boise, ID</ion-select-option>
+          </ion-select>
+        </ion-item>
+        <ion-item>
+          <ion-label>Temperature</ion-label>
+          <ion-select slot="end" interface="action-sheet" placeholder="Fahrenheit">
+            <ion-select-option value="celsius">Celsius</ion-select-option>
+            <ion-select-option value="fahrenheit">Fahrenheit</ion-select-option>
+          </ion-select>
+        </ion-item>
+        <ion-iten class="temp">
+          <ion-button slot="end" fill="outline">Login</ion-button>
+          <ion-button slot="end" fill="outline">Sign Up</ion-button>
+        </ion-iten>
+
+      </ion-list>
+    </ion-content>
+    
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonButton } from '@ionic/vue';
+</script>
+
+<style scoped>
+
+.temp {
+    display: flex;
+    justify-content: center;
+    margin-top: 5rem;
+  }
+
+
+
+</style>
