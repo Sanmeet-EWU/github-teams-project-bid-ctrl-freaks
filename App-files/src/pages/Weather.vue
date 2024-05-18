c6d0a18005cb5708bf3fadc904927ec7e7f6682d
Logan Taggart <ltaggart1@ewu.edu>
2024-05-17 19:53:15 -0700

    Update Weather.vue

--- App-files/src/pages/Weather.vue
+++ App-files/src/pages/Weather.vue
@@ -1,121 +1,239 @@
 <template>
-    <ion-page>
+  <ion-page>
     <ion-header class="ion-no-border" mode="ios">
-      <ion-toolbar class ="ion-padding-start ion-padding-end">
+      <ion-toolbar class="ion-padding-start ion-padding-end">
         <ion-title>Weather</ion-title>
-        
-        
-          <ion-icon :icon="searchSharp" />
         <span slot="end">
-          <ion-icon :icon="notificationsSharp" />
+          <ion-icon :icon="searchSharp" />
         </span>
       </ion-toolbar>
     </ion-header>
 
     <ion-content>
-        <h4 class="location">Spokane, WA</h4> <!-- replace with location component -->
-        <div class="weather-icon"> <!-- replace with weather icon to change accordingly -->
-          <img src="../images/sunny.png">
-        </div>
-        <h2 class="weather">Sunny</h2> <!-- replace with weather component -->
-        <h1 class="temperature">25°C</h1> <!-- replace with temperature component -->
+      <h4 class="location">Spokane, WA</h4>
+      <div class="weather-icon">
+        <img src="../images/sunny.png">
+      </div>
+      <h2 class="weather">Sunny</h2>
+      <h1 class="temperature">25°F</h1>
 
 
-        <div class="infoTab ion-padding">
-        <ion-card class="ion-padding"> <!-- replace with weather details component -->
-          <div class="basicinfo">        
+      <div class="infoTab ion-padding">
+        <ion-card class="ion-padding">
+          <div class="basicinfo">
             <h5>Wind</h5>
-            <h7>W 1000 mph</h7>
+            <h6>10 mph</h6>
           </div>
         </ion-card>
-        <ion-card class="ion-padding"> 
-          <div class="basicinfo">        
+        <ion-card class="ion-padding">
+          <div class="basicinfo">
             <h5>Feels like</h5>
-            <h7>69°C</h7>
+            <h6>69°C</h6>
           </div>
         </ion-card>
         <ion-card class="ion-padding">
-          <div class="basicinfo">        
-            <h5>Humidity</h5>
-            <h7>8000 %</h7>
+          <div class="basicinfo">
+            <h5>Feels like</h5>
+            <h6>69°C</h6>
           </div>
         </ion-card>
-        <ion-card class="ion-padding">
-          <div class="basicinfo">        
-            <h5>Rain</h5>
-            <h7>0%</h7>
-          </div>
-        </ion-card>
-        <ion-card class="ion-padding">
-          <div class="basicinfo">        
-            <h5>Cloud</h5>
-            <h7>0%</h7>
-          </div>
-        </ion-card>
-        <ion-card class="ion-padding">
-          <div class="basicinfo">        
-            <h5>Lightning</h5>
-            <h7>666 %</h7>
-          </div>
-        </ion-card>
-      </div>  
-
-      <div class="hourly">
-      <h6 class="ion-padding-start">Today Forecast:</h6>
-        <div class="infoTab ion-padding">
-          <ion-card class="ion-padding">        
-            <h4>00:00</h4>
-            <img class="hourimg" src="../images/sunny.png">
-            <h3>25°C</h3>
-          </ion-card>
-          <ion-card class="ion-padding">        
-            <h4>01:00</h4>
-            <img class="hourimg" src="../images/sunny.png">
-            <h3>25°C</h3>
-          </ion-card>
-          <ion-card class="ion-padding">        
-            <h4>02:00</h4>
-            <img class="hourimg" src="../images/sunny.png">
-            <h3>25°C</h3>
-          </ion-card>
-          <ion-card class="ion-padding">        
-            <h4>03:00</h4>
-            <img class="hourimg" src="../images/sunny.png">
-            <h3>25°C</h3>
-          </ion-card>
-          <ion-card class="ion-padding">        
-            <h4>04:00</h4>
-            <img class="hourimg" src="../images/sunny.png">
-            <h3>25°C</h3>
-          </ion-card>
-          <ion-card class="ion-padding">        
-            <h4>05:00</h4>
-            <img class="hourimg" src="../images/sunny.png">
-            <h3>25°C</h3>
-          </ion-card>
-          <ion-card class="ion-padding">        
-            <h4>06:00</h4>
-            <img class="hourimg" src="../images/sunny.png">
-            <h3>25°C</h3>
-          </ion-card>
-          <ion-card class="ion-padding">        
-            <h4>07:00</h4>
-            <img class="hourimg" src="../images/sunny.png">
-            <h3>25°C</h3>
-          </ion-card>
-        </div>
-
-
       </div>
 
+      <div class="hourly">
+        <h6 class="ion-padding-start">Today's Forecast:</h6>
+        <div class="infoTab ion-padding">
+          <ion-card class="ion-padding" v-for="(temp, index) in temp" :key="index">
+            <!--Iterates through array of 24 hourly values, makes card for each one-->
+            <h4>{{ formatHour(index) }}</h4>
+            <!--Calls formatHour function in script based on index to determine if time is AM or PM and print time-->
+            <img class="cardimg" src="../images/sunny.png" />
+            <h3>{{ Math.round(temp) }}°F</h3> <!--Prints rounded temperature variable from script-->
+          </ion-card>
+        </div>
 
+        <div class="hourly">
+          <h6 class="ion-padding-start">Daily Forecast:</h6>
+          <div class="infoTab ion-padding">
+            <ion-card class="ion-padding">
+              <h4>Monday</h4>
+              <img class="cardimg" src="../images/sunny.png">
+              <h3>25°C</h3>
+            </ion-card>
+            <ion-card class="ion-padding">
+              <h4>Tuesday</h4>
+              <img class="cardimg" src="../images/sunny.png">
+              <h3>25°C</h3>
+            </ion-card>
+            <ion-card class="ion-padding">
+              <h4>Wednesday</h4>
+              <img class="cardimg" src="../images/sunny.png">
+              <h3>25°C</h3>
+            </ion-card>
+            <ion-card class="ion-padding">
+              <h4>Thursday</h4>
+              <img class="cardimg" src="../images/sunny.png">
+              <h3>25°C</h3>
+            </ion-card>
+            <ion-card class="ion-padding">
+              <h4>Friday</h4>
+              <img class="cardimg" src="../images/sunny.png">
+              <h3>25°C</h3>
+            </ion-card>
+            <ion-card class="ion-padding">
+              <h4>Saturday</h4>
+              <img class="cardimg" src="../images/sunny.png">
+              <h3>25°C</h3>
+            </ion-card>
+            <ion-card class="ion-padding">
+              <h4>Sunday</h4>
+              <img class="cardimg" src="../images/sunny.png">
+              <h3>25°C</h3>
+            </ion-card>
+          </div>
+        </div>
+      </div>
     </ion-content>
-    </ion-page>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonCard} from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonCard } from '@ionic/vue';
 import { notificationsSharp, searchSharp } from 'ionicons/icons';
+
+import { ref } from 'vue' //Imported this
+
+//This is a temp json data structure for testing
+const data = {
+  "latitude": 52.52,
+  "longitude": 13.419998,
+  "generationtime_ms": 0.06198883056640625,
+  "utc_offset_seconds": 0,
+  "timezone": "GMT",
+  "timezone_abbreviation": "GMT",
+  "elevation": 38.0,
+  "hourly_units": {
+    "time": "iso8601",
+    "temperature_2m": "°F",
+    "weather_code": "wmo code",
+    "wind_speed_10m": "mp/h"
+  },
+  "hourly": {
+    "time": [
+      "2024-05-18T00:00",
+      "2024-05-18T01:00",
+      "2024-05-18T02:00",
+      "2024-05-18T03:00",
+      "2024-05-18T04:00",
+      "2024-05-18T05:00",
+      "2024-05-18T06:00",
+      "2024-05-18T07:00",
+      "2024-05-18T08:00",
+      "2024-05-18T09:00",
+      "2024-05-18T10:00",
+      "2024-05-18T11:00",
+      "2024-05-18T12:00",
+      "2024-05-18T13:00",
+      "2024-05-18T14:00",
+      "2024-05-18T15:00",
+      "2024-05-18T16:00",
+      "2024-05-18T17:00",
+      "2024-05-18T18:00",
+      "2024-05-18T19:00",
+      "2024-05-18T20:00",
+      "2024-05-18T21:00",
+      "2024-05-18T22:00",
+      "2024-05-18T23:00"
+    ],
+    "temperature_2m": [
+      60.3,
+      59.5,
+      58.6,
+      57.3,
+      56.5,
+      57.3,
+      58.7,
+      61.2,
+      63.0,
+      64.9,
+      66.6,
+      65.9,
+      66.6,
+      66.9,
+      65.7,
+      63.7,
+      64.2,
+      64.0,
+      63.0,
+      61.6,
+      60.2,
+      59.1,
+      58.4,
+      57.4
+    ],
+    "weather_code": [
+      2,
+      3,
+      3,
+      2,
+      3,
+      3,
+      2,
+      2,
+      3,
+      2,
+      3,
+      3,
+      3,
+      3,
+      61,
+      61,
+      80,
+      2,
+      3,
+      2,
+      3,
+      3,
+      2,
+      2
+    ],
+    "wind_speed_10m": [
+      6.7,
+      6.8,
+      7.4,
+      6.9,
+      6.5,
+      6.8,
+      7.2,
+      8.0,
+      8.8,
+      8.1,
+      9.5,
+      8.5,
+      9.2,
+      8.6,
+      6.3,
+      7.3,
+      3.0,
+      4.9,
+      6.1,
+      3.3,
+      3.0,
+      3.5,
+      3.0,
+      3.4
+    ]
+  }
+}
+
+// Stores reference to the temperature array that is iterated through in the template
+const temp = ref(data.hourly.temperature_2m);
+
+// Formats hour time based on the index
+function formatHour(index) {
+  const hour = index % 12 === 0 ? 12 : index % 12;
+  const ampm = index < 12 ? 'AM' : 'PM';
+  return `${hour}:00 ${ampm}`;
+}
 </script>
 
 <style scoped>
@@ -128,7 +246,7 @@ ion-content {
   --background-repeat: none;
   --background-attachment: none;
   --background-blend-mode: none;
-  }
+}
 
 
 .location {
@@ -143,7 +261,7 @@ ion-content {
   height: 14rem;
   display: flex;
   justify-content: center;
-  margin-top : 2rem;
+  margin-top: 2rem;
 }
 
 .weather {
@@ -166,7 +284,7 @@ h5 {
   font-weight: bold;
 }
 
-h7 {
+h6 {
   color: black;
   font-size: 1rem;
   font-weight: normal;
@@ -189,6 +307,7 @@ ion-card {
   border-radius: 1rem;
   margin: 0px;
   margin-right: 0.5rem;
+  width: 115px;
 }
 
 h4 {
@@ -196,6 +315,7 @@ h4 {
   font-size: 1rem;
   font-weight: bold;
   margin: 0px;
+  margin-bottom: 10px;
   text-align: center;
 }
 
@@ -216,12 +336,9 @@ h6 {
   margin-top: 1rem;
 }
 
-.hourimg {
+.cardimg {
   height: 3rem;
-  display: flex;
-  justify-content: center;
-  margin-top : 0px;
+  display: block;
+  margin: 0 auto;
 }
-
-
 </style>
