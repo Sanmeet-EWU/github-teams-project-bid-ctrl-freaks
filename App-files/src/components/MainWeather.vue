c560ea6ce18c89a482f2cc74258019575cff08b8
Eric Vo <tvo12@ewu.edu>
2024-06-03 17:42:35 -0700

    Change MainWeather to use Icon from GetIcon component

--- App-files/src/components/MainWeather.vue
+++ App-files/src/components/MainWeather.vue
@@ -1,7 +1,7 @@
 <template>
   <h3 class="location">{{ City }}</h3>
   <div class="weather-icon">
-    <img :src="weatherData.icon" alt="Weather icon">
+    <GetIcon :weatherCode="weatherData.icon" :hourOfDay="date.getHours()" />
   </div>
   <h2 class="weather">{{ weatherData.name }}</h2>
   <h1 class="temperature">{{ temperature }}</h1>
@@ -13,31 +13,7 @@
 </template>
 
 <script setup lang="ts">
-import SunnyLightCloud from "../images/animated_icon/light_cloudy_day.svg"
-import SunnyMediumCloud from '../images/animated_icon/medium_cloudy_day.svg';
-import SunnyHeavyCloud from '../images/animated_icon/heavy_cloudy_day.svg'; //not used
-import NightLightCloud from '../images/animated_icon/light_cloudy_night.svg';
-import NightMediumCloud from '../images/animated_icon/medium_cloudy_night.svg';
-import NightHeavyCloud from '../images/animated_icon/heavy_cloudy_night.svg'; //not used
-import Cloudy from '../images/animated_icon/only_cloudy.svg';
-import SunnyLightRain from "../images/animated_icon/sunny_light_rain.svg";
-import LightSunnyLightRain from "../images/animated_icon/lightsun_light_rain.svg";
-import LightSunnyMediumRain from "../images/animated_icon/lightsun_medium_rain.svg"; //not used
-import LightRain from "../images/animated_icon/light_rain.svg";
-import MediumRain from "../images/animated_icon/medium_rain.svg";
-import HeavyRain from "../images/animated_icon/heavy_rain.svg";
-import Drizzle from "../images/animated_icon/drizzle.svg";
-import SunnyLightSnow from "../images/animated_icon/sunny_light_snow.svg";
-import LightSunnyLightSnow from "../images/animated_icon/lightsun_light_snow.svg";
-import LightSunnyMediumSnow from "../images/animated_icon/lightsun_medium_snow.svg";
-import LightSnow from "../images/animated_icon/light_snow.svg";
-import MediumSnow from "../images/animated_icon/medium_snow.svg";
-import HeavySnow from "../images/animated_icon/heavy_snow.svg";
-import Sunny from '../images/animated_icon/sunny.svg';
-import Night from '../images/animated_icon/night.svg';
-import Thunder from '../images/animated_icon/thunder.svg';
-import Foggy from '../images/animated_icon/Foggy.gif';
-
+import GetIcon from './GetIcon.vue';
 
 
 import { ref, computed } from 'vue'
@@ -54,50 +30,46 @@ const props = defineProps<{
 //get hour of day to output day or night weather icon
 const date = new Date();
 
-const weatherMap: { [key: number]: () => { icon: string; name: string; }; default: () => { icon: string; name: string; }; } = {
-  1: () => date.getHours() < 16 ? { icon: Sunny, name: "Clear Sky" } : { icon: Night, name: "Clear Sky" },
-  2: () => date.getHours() < 16 ? { icon: SunnyLightCloud, name: "Mainly Clear" } : { icon: NightLightCloud, name: "Mainly Clear" },
-  3: () => date.getHours() < 16 ? { icon: SunnyMediumCloud, name: "Partially cloudy" } : { icon: NightMediumCloud, name: "Partially cloudy" },
-  4: () => ({ icon: Cloudy, name: "Overcast" }),
-  5: () => ({ icon: Foggy, name: "Foggy" }),
-  6: () => ({ icon: Drizzle, name: "Drizzle" }),
+const weatherMap: { [key: number]: () => { icon: number; name: string; }; default: () => { icon: number; name: string; }; } = {
+  1: () => ({ icon: 1, name: "Clear Sky" }),
+  2: () => ({ icon: 2, name: "Mainly Clear" }),
+  3: () => ({ icon: 3, name: "Partially cloudy" }),
+  4: () => ({ icon: 4, name: "Overcast" }),
+  5: () => ({ icon: 5, name: "Foggy" }),
+  6: () => ({ icon: 6, name: "Drizzle" }),
 
-  45: () => ({ icon: Foggy, name: "Foggy" }),
-  48: () => ({ icon: Foggy, name: "Foggy" }),
+  45: () => ({ icon: 45, name: "Foggy" }),
+  48: () => ({ icon: 48, name: "Foggy" }),
 
+  51: () => ({ icon: 51, name: "Light Drizzle" }),
+  56: () => ({ icon: 56, name: "Light Freezing Drizzle" }),
 
-  51: () => date.getHours() < 16 ? { icon: LightSunnyLightRain, name: "Light Drizzle" } : { icon: LightRain, name: "Light Drizzle" },
-  56: () => date.getHours() < 16 ? { icon: LightSunnyLightRain, name: "Light Freezing Drizzle" } : { icon: LightRain, name: "Light Freezing Drizzle" },
+  61: () => ({ icon: 61, name: "Light Rain" }),
+  66: () => ({ icon: 66, name: "Light Freezing Rain" }),
+  80: () => ({ icon: 80, name: "Slight Rain Shower" }),
 
+  53: () => ({ icon: 53, name: "Moderate Drizzle" }),
+  63: () => ({ icon: 63, name: "Moderate Rain" }),
+  81: () => ({ icon: 81, name: "Moderate Rain Shower" }),
 
-  61: () => date.getHours() < 16 ? { icon: LightSunnyLightRain, name: "Light Rain" } : { icon: LightRain, name: "Light Rain" },
-  66: () => date.getHours() < 16 ? { icon: LightSunnyLightRain, name: "Light Freezing Rain" } : { icon: LightRain, name: "Light Freezing Rain" },
-  80: () => date.getHours() < 16 ? { icon: LightSunnyLightRain, name: "Slight Rain Shower" } : { icon: LightRain, name: "Slight Rain Shower" },
+  55: () => ({ icon: 55, name: "Heavy Drizzle" }),
+  65: () => ({ icon: 65, name: "Heavy Rain" }),
+  67: () => ({ icon: 67, name: "Heavy Freezing Rain" }),
+  82: () => ({ icon: 82, name: "Violent Rain Shower" }),
 
-  53: () => date.getHours() < 16 ? { icon: SunnyLightRain, name: "Moderate Drizzle" } : { icon: MediumRain, name: "Moderate Drizzle" },
-  63: () => date.getHours() < 16 ? { icon: SunnyLightRain, name: "Moderate Rain" } : { icon: MediumRain, name: "Moderate Rain" },
-  81: () => date.getHours() < 16 ? { icon: SunnyLightRain, name: "Moderate Rain Shower" } : { icon: MediumRain, name: "Moderate Rain Shower" },
+  71: () => ({ icon: 71, name: "Slight Snow" }),
+  73: () => ({ icon: 73, name: "Moderate Snow" }),
+  75: () => ({ icon: 75, name: "Heavy Snowfall" }),
 
+  77: () => ({ icon: 77, name: "Snow Grains" }),
 
-  55: () => ({ icon: Drizzle, name: "Heavy Drizzle" }),
-  65: () => ({ icon: HeavyRain, name: "Heavy Rain" }),
-  67: () => ({ icon: HeavyRain, name: "Heavy Freezing Rain" }),
-  82: () => ({ icon: HeavyRain, name: "Violent Rain Shower" }),
+  85: () => ({ icon: 85, name: "Light Snow Shower" }),
+  86: () => ({ icon: 86, name: "Heavy Snow Shower" }),
+  95: () => ({ icon: 95, name: "Thunderstorm" }),
+  96: () => ({ icon: 96, name: "Thunderstorm with Slight Hail" }),
+  99: () => ({ icon: 99, name: "Thunderstorm with Heavy Hail" }),
 
-
-  71: () => date.getHours() < 16 ? { icon: LightSunnyLightSnow, name: "Slight Snow" } : { icon: LightSnow, name: "Slight Snow" },
-  73: () => date.getHours() < 16 ? { icon: LightSunnyMediumSnow, name: "Moderate Snow" } : { icon: MediumSnow, name: "Moderate Snow" },
-  75: () => ({ icon: HeavySnow, name: "Heavy Snowfall" }),
-
-  77: () => date.getHours() < 16 ? { icon: SunnyLightSnow, name: "Moderate Rain Shower" } : { icon: MediumSnow, name: "Snow Grains" },
-
-  85: () => date.getHours() < 16 ? { icon: LightSunnyLightSnow, name: "Moderate Rain Shower" } : { icon: LightSnow, name: "Light Snow Shower" },
-  86: () => ({ icon: HeavySnow, name: "Heavy Snow Shower" }),
-  95: () => ({ icon: Thunder, name: "Thunderstorm" }),
-  96: () => ({ icon: Thunder, name: "Thunderstorm with Slight Hail" }),
-  99: () => ({ icon: Thunder, name: "Thunderstorm with Heavy Hail" }),
-
-  default: () => date.getHours() < 16 ? { icon: Sunny, name: "Clear Sky" } : { icon: Night, name: "Clear Sky" },
+  default: () => ({ icon: 1, name: "Clear Sky" }),
 }
 
 
